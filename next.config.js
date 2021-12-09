5b3bf8f4cbcd2ab770c0483d81cd9a0e55c417d6
karim-jaypay <karim@jaypayagency.com>
2021-12-09 19:05:15 +0200

    t

--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ['https://everestcmstrapi.jaypay.co.uk'],
+    loader: 'imgix',
+    path: ''
+  },
+
+  // For Subroutes in build folder
+  trailingSlash: true,
+
+  // Restore page scroll position when going back
+  experimental: {
+    scrollRestoration: true,
+  },
 }
